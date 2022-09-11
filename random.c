2f2451cdb0464853cd52473b7789d0648218a07b
rusnuker <49472785+rusnuker@users.noreply.github.com>
2022-09-11 18:23:37 +0300

    Update random.c
    functions -> macros

--- random.c
+++ random.c
@@ -1,11 +1,4 @@
 #include<time.h>
 
-int SetRandSeed() {
-	// Call this function BEFORE calling random().
-	srand(time(0)+4*92);
-	return 0;
-}
-
-int random(int lower, int higher) {
-	return lower+rand()%(higher-lower+1);
-}
+#define SetRandSeed() srand(time(0)+4*92)
+#define random(a,b) a+rand()%(b-a+1)
